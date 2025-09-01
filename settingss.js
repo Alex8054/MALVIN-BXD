fb6d2580d5a336a2e45094bc94c47fee50c93da8
Alex8054 <alexboy80547@gmail.com>
2025-09-01 10:36:46 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Rr10gBYA#3L_7w_TbaCNBOHy5L0wysKBlIOxb-z6kNlGdsdnWi8I",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "989136715476", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
